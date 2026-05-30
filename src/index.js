8a90ed64bd5836653d7c6bb008ec3d674adb9774
rexシ <fatuyrahman0@gmail.com>
2026-05-31 01:13:46 +0700

    Change React and ReactDOM imports to CDN
    Updated import statements to use CDN links for React and ReactDOM.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import IserToolkit from './src/IserToolkit.jsx'; // Sesuaikan path jika perlu
+import React from 'https://esm.sh/react@18.3.1';
+import ReactDOM from 'https://esm.sh/react-dom@18.3.1/client';
+import IserToolkit from './IserToolkit.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(IserToolkit));
+root.render(React.createElement(IserToolkit.default || IserToolkit));
