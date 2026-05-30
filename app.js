1b25614b4c9b1eb00ddfbe5d33cd2f881fe5f875
rexシ <fatuyrahman0@gmail.com>
2026-05-31 01:20:17 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// app.js - Tempat menaruh script tambahan
-console.log("IserToolkit System Initialized");
-
-// Contoh fungsi global yang bisa dipanggil dari mana saja
-window.logSystem = () => {
-    console.log("Sistem berjalan dengan lancar");
-};
