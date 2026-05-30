ea1f6f6c343b0397a33049589fb0df200cb794f8
rexシ <fatuyrahman0@gmail.com>
2026-05-31 01:08:10 +0700

    Add initial React setup in index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IserToolkit from './src/IserToolkit.jsx'; // Sesuaikan path jika perlu
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(IserToolkit));
