8c106a68da5f5ccbca472e2a90157a60a0090946
rexシ <fatuyrahman0@gmail.com>
2026-05-31 01:36:40 +0700

    Update Vite config base path and add build options
    Change base path to './' for compatibility and add Rollup input configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/IserToolkit/'
+  base: './', // Pakai ./ agar tidak masalah dengan path
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 });
