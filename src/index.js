2909129243f8fbccaa60bff249d6972e754076e8
rexシ <fatuyrahman0@gmail.com>
2026-05-31 01:16:37 +0700

    Change React and ReactDOM imports to local modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'https://esm.sh/react@18.3.1';
-import ReactDOM from 'https://esm.sh/react-dom@18.3.1/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import IserToolkit from './IserToolkit.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
