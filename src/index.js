ab4c4dd95d176a06d2eab39c8f811a38e123dfd8
rexシ <fatuyrahman0@gmail.com>
2026-05-31 01:20:32 +0700

    Delete src/index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import IserToolkit from './IserToolkit.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(IserToolkit.default || IserToolkit));
